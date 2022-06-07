b8b0fabdc1835ed33b5ab0c005cfe94bfb12ca1e
WDamaris <damarisshish@gmail.com>
2022-06-08 00:18:37 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ function distanceFromHqInBlocks(block) {
     return 42-block;
  }
 }
+
 function distanceFromHqInFeet(feet){
     return distanceFromHqInBlocks(feet)*264
-
 }
-
 
 function distanceTravelledInFeet(start, destination){
+ if (destination>start){
+     return  (destination-start)*264;
+ }else{
+     return (start- destination)*264;
+ }
+}
+function calculatesFarePrice(start, destination){
+ const feet=distanceTravelledInFeet(start, destination);
+    if (feet<=400){
+      return 0;
+    }else if(feet>400 && feet<=2000){
+      return 0.02*(feet-400);
+    }else if (feet>200 && feet<=2500){
+        return 25;
+    }else{
+     return "cannot travel that far";
+    }
 
 }
-function calculatesFarePrice(fare){}
